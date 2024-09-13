f95fe960649a346f42aa50602b2e2cecb4f40dd0
fcmenkcv <fcmenkcv@gmail.com>
2024-09-13 20:59:40 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0702;
+
+/*
+Максимальное целое
+*/
+
+public class Solution {
+    // напишите тут ваш код
+    byte a = 127;
+    short b = 32767;
+    int c = 2147483647;
+    long d = 9223372036854775807l;
+}
